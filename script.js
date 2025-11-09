32ae5062533d8affc66f3034de4345cef1581851
Rusith <rusithbandara20021204@gmail.com>
2025-11-09 08:23:11 +0530

    add funny feature

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const hourEl = document.querySelector('.hour')
 const minuteEl = document.querySelector('.minute')
 const secondEl = document.querySelector('.second')
-const timeEl = document.querySelector('.time')
+// const timeEl = document.querySelector('.time')
 const dateEl = document.querySelector('.date')
 const toggle = document.querySelector('.toggle')
 
@@ -37,6 +37,35 @@ function setTime() {
     timeEl.innerHTML = `${hoursForClock}:${minutes < 10 ? `0${minutes}` : minutes} ${ampm}`
     dateEl.innerHTML = `${days[day]}, ${months[month]} <span class="circle">${date}</span>`
 }
+
+// Fun Easter Egg: Click the clock center for a surprise message
+const centerPoint = document.querySelector('.center-point');
+const timeEl = document.querySelector('.time');
+
+const messages = [
+  "Time for coffee! ☕",
+  "You're doing great! 👍",
+  "Is it lunch time yet? 🍕",
+  "Tick Tock, Tick Tock...",
+  "Still awake? 😴",
+  "Productivity mode: ON! 💻",
+  "This clock is judging you. 👀"
+];
+
+centerPoint.addEventListener('click', () => {
+  const randomMessage = messages[Math.floor(Math.random() * messages.length)];
+  const originalTime = timeEl.innerHTML;
+  
+  // Display the funny message
+  timeEl.innerHTML = randomMessage;
+  timeEl.style.color = '#e74c3c'; // Change color for emphasis
+
+  // Revert back to the actual time after 2 seconds
+  setTimeout(() => {
+    timeEl.innerHTML = originalTime;
+    timeEl.style.color = ''; // Revert to original color
+  }, 2000);
+});
 
 // StackOverflow https://stackoverflow.com/questions/10756313/javascript-jquery-map-a-range-of-numbers-to-another-range-of-numbers
 const scale = (num, in_min, in_max, out_min, out_max) => {
